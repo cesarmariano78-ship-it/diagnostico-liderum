b0cd423a36e254df8ec2ef952d3596d9f3657e9d
cesarmariano78-ship-it <cesarmariano78@gmail.com>
2026-01-07 23:07:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -659,9 +659,7 @@ elif st.session_state.etapa == "questoes":
 
    
 
-    st.markdown("<div class='divider-brasil'></div>", unsafe_allow_html=True)
-    st.markdown("<p class='small'>Instrução: clique em cada dimensão para abrir as perguntas. Responda todas as 45 para liberar o diagnóstico.</p>", unsafe_allow_html=True)
-
+    
     
 
 
